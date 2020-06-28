541c27eef1f7305850a9b7535a7d959b0f3d4751
isaacdvid1596 <isaacdvid15.96@gmail.com>
2020-06-28 09:48:31 -0600

    Add files via upload
    downward movement enabled , after image bug , diagonal move instead of downward move

--- KeypadSampleFSM.v
+++ KeypadSampleFSM.v
@@ -7,8 +7,8 @@ module KeypadSampleFSM(
     output reg vga_we,
     output reg [15:0] vga_data
 );
-    reg [4:0] cs /*verilator public*/; // Current state
-    reg [4:0] ns; // Next state
+    reg [5:0] cs /*verilator public*/; // Current state
+    reg [5:0] ns; // Next state
 
     reg [6:0] row /*verilator public*/; // Current row
     reg [6:0] col /*verilator public*/; // Current col
@@ -22,24 +22,43 @@ module KeypadSampleFSM(
     // VGA Write Enable
         always @ (*) begin
         case(cs)
-        5'd0:  vga_we = 1'd0;
-        5'd1:  vga_we = 1'd0;
-        5'd2:  vga_we = 1'd1;
-        5'd3:  vga_we = 1'd0;
-        5'd4:  vga_we = 1'd1;
-        5'd5:  vga_we = 1'd0;
-        5'd6:  vga_we = 1'd0;
-        5'd7:  vga_we = 1'd0;
-        5'd8:  vga_we = 1'd1;
-        5'd9:  vga_we = 1'd1;
-        5'd10: vga_we = 1'd0;
-        5'd11: vga_we = 1'd0;
-        5'd12: vga_we = 1'd1;
-        5'd13: vga_we = 1'd0;
-        5'd14: vga_we = 1'd1;
-        5'd15: vga_we = 1'd0;
-        5'd16: vga_we = 1'd0;
-        5'd17: vga_we = 1'd0;
+        6'd0:  vga_we = 1'd0;
+        6'd1:  vga_we = 1'd0;
+        6'd2:  vga_we = 1'd1;
+        6'd3:  vga_we = 1'd0;
+        6'd4:  vga_we = 1'd1;
+        6'd5:  vga_we = 1'd0;
+        6'd6:  vga_we = 1'd0;
+        6'd7:  vga_we = 1'd0;
+        6'd8:  vga_we = 1'd1;
+        6'd9:  vga_we = 1'd1;
+
+        6'd10: vga_we = 1'd0;
+        6'd11: vga_we = 1'd0;
+        6'd12: vga_we = 1'd1;
+        6'd13: vga_we = 1'd0;
+        6'd14: vga_we = 1'd1;
+        6'd15: vga_we = 1'd0;
+        6'd16: vga_we = 1'd0;
+        6'd17: vga_we = 1'd0;
+        
+        6'd18: vga_we = 1'd0;
+        6'd19: vga_we = 1'd0;
+        6'd20: vga_we = 1'd1;
+        6'd21: vga_we = 1'd0;
+        6'd22: vga_we = 1'd1;
+        6'd23: vga_we = 1'd0;
+        6'd24: vga_we = 1'd0;
+        6'd25: vga_we = 1'd0;
+
+        6'd26: vga_we = 1'd1;
+        6'd27: vga_we = 1'd0;
+        6'd28: vga_we = 1'd1;
+        6'd29: vga_we = 1'd0;
+        6'd30: vga_we = 1'd1;
+        6'd31: vga_we = 1'd0;
+        6'd32: vga_we = 1'd0;
+        6'd33: vga_we = 1'd0;
 
         default:
         vga_we = 1'd0;
@@ -50,42 +69,109 @@ module KeypadSampleFSM(
     // VGA data
         always @ (*)begin
         case(cs)
-        5'd0: 
+        6'd0: 
         vga_data =  16'hx;
-        5'd1: 
+
+        6'd1: 
         vga_data =  16'hx;
-        5'd2:
+
+        6'd2:
         vga_data =  16'h0e01;
-        5'd3:
+
+        6'd3:
         vga_data =  16'h0e02;
-        5'd4:
+
+        6'd4:
         vga_data =  16'h0e02;
-        5'd5:
+
+        6'd5:
         vga_data =  16'hx;
-        5'd6:
+
+        6'd6:
         vga_data =  16'hx;
-        5'd7:
+
+        6'd7:
         vga_data =  16'hx;
-        5'd8:
+
+        6'd8:
         vga_data =  16'h0e02;
-        5'd9:
+
+        6'd9:
         vga_data =  16'h0e02;
-        5'd10:
+
+        6'd10:
         vga_data =  16'hx;
-        5'd11:
+
+        6'd11:
         vga_data =  16'h0e01;
-        5'd12:
+
+        6'd12:
         vga_data =  16'h0e01;
-        5'd13:
+
+        6'd13:
         vga_data =  16'h0e02;
-        5'd14:
+
+        6'd14:
         vga_data =  16'h0e02;
-        5'd15:
+
+        6'd15:
         vga_data =  16'hx;
-        5'd16:
+
+        6'd16:
         vga_data =  16'hx;
-        5'd17:
+
+        6'd17:
         vga_data =  16'hx;
+
+        6'd18:
+        vga_data =  16'hx;
+
+        6'd19:
+        vga_data =  16'h0e04;
+
+        6'd20:
+        vga_data =  16'h0e04;
+
+        6'd21:
+        vga_data =  16'h0e03;
+
+        6'd22:
+        vga_data =  16'h0e03;
+
+        6'd23:
+        vga_data =  16'hx;
+
+        6'd24:
+        vga_data =  16'hx;
+
+        6'd25:
+        vga_data =  16'hx;
+
+        6'd26:
+        vga_data =  16'h0e07;
+
+        6'd27:
+        vga_data =  16'h0e08;
+
+        6'd28:
+        vga_data =  16'h0e08;
+
+        6'd29:
+        vga_data =  16'h0e07;
+
+        6'd30:
+        vga_data =  16'h0e07;
+
+        6'd31:
+        vga_data =  16'hx;
+
+        6'd32:
+        vga_data =  16'hx;
+
+        6'd33:
+        vga_data =  16'hx;
+
+        
         default:begin
         vga_data  = 16'hx;
         end
@@ -102,51 +188,73 @@ module KeypadSampleFSM(
         
         case (cs)
 
-        5'd0: ns = 5'd1;
-        5'd1: ns = 5'd2;
-        5'd2: ns = 5'd3;
-        5'd3: ns = 5'd4;
-        5'd4: ns = 5'd5;
+        6'd0: ns = 6'd1;
+        6'd1: ns = 6'd2;
+        6'd2: ns = 6'd3;
+        6'd3: ns = 6'd4;
+        6'd4: ns = 6'd5;
         
-        5'd5: 
+        6'd5: 
         begin
             if(keypad[0])
-            ns = 5'd6;
+            ns = 6'd6;
             else if(keypad[1])
-            ns = 5'd7;
+            ns = 6'd7;
             else if(keypad[2])
-            ns = 5'd8;
+            ns = 6'd8;
             else if(keypad[3])
-            ns = 5'd9;
+            ns = 6'd9;
             else 
-            ns = 5'd5;
+            ns = 6'd5;
         end
 
         //izquierda
-        5'd6: ns = 5'd6;
+        6'd6: ns = (col > 2)? 6'd18 : 6'd23;
+        //true
+        6'd18: ns = 6'd19;
+        6'd19: ns = 6'd20;
+        6'd20: ns = 6'd21;
+        6'd21: ns = 6'd22;
+        6'd22: ns = 6'd5;
+        //false
+        6'd23: ns = (row<28)? 6'd24 : 6'd25;
+        6'd24: ns = 6'd18;
+        6'd25: ns = 6'd18;
 
 
         //derecha
-        5'd7:  ns = (col<78)? 5'd10 : 5'd15;
+        6'd7:  ns = (col<78)? 6'd10 : 6'd15;
         //true
-        5'd10: ns = 5'd11;
-        5'd11: ns = 5'd12;
-        5'd12: ns = 5'd13;
-        5'd13: ns = 5'd14;
-        5'd14: ns = 5'd5;
+        6'd10: ns = 6'd11;
+        6'd11: ns = 6'd12;
+        6'd12: ns = 6'd13;
+        6'd13: ns = 6'd14;
+        6'd14: ns = 6'd5;
         //false
-        5'd15: ns = (row<28)? 5'd16 : 5'd17;
-        5'd16: ns = 5'd10;
-        5'd17: ns = 5'd10;
+        6'd15: ns = (row<28)? 6'd16 : 6'd17;
+        6'd16: ns = 6'd10;
+        6'd17: ns = 6'd10;
+
+
 
         //abajo
-        5'd8: ns = 5'd8;
+        6'd8:  ns = (row<28)? 6'd26 : 6'd31;
+        //true
+        6'd26: ns = 6'd27;
+        6'd27: ns = 6'd28;
+        6'd28: ns = 6'd29;
+        6'd29: ns = 6'd30;
+        6'd30: ns = 6'd5;
+        //false
+        6'd31: ns = (col>2)? 6'd32 : 6'd33;
+        6'd32: ns = 6'd26;
+        6'd33: ns = 6'd26;
 
         //arriba
-        5'd9: ns = 5'd9;
+        6'd9: ns = 6'd9;
 
         default:
-        ns = 5'd0; 
+        ns = 6'd0; 
 
         endcase
 
@@ -155,7 +263,7 @@ module KeypadSampleFSM(
     always @ (posedge clk)
     begin
         if (rst)
-            cs <= 5'd0;
+            cs <= 6'd0;
         else
             cs <= ns;
     end
@@ -164,11 +272,17 @@ module KeypadSampleFSM(
     always @ (posedge clk)
     begin
         case (cs)
-            5'd0:  col <= 7'd10;
-            5'd2:  col <= col + 7'd1;
-            5'd10: col <= col + 7'd1;
-            5'd12: col <= col + 7'd1;
-            5'd15: col <= 7'd0;
+            6'd0:  col <= 7'd10;
+            6'd2:  col <= col + 7'd1;
+            6'd10: col <= col + 7'd1;
+            6'd12: col <= col + 7'd1;
+            6'd15: col <= 7'd0;
+            6'd18: col <= col - 7'd1;
+            6'd20: col <= col - 7'd1;
+            6'd23: col <= 7'd78;
+            6'd28: col <= col - 7'd1;
+            6'd32: col <= col - 7'd1;
+            6'd33: col <= 7'd78;
             default:
                 col <= col;
         endcase
@@ -178,9 +292,13 @@ module KeypadSampleFSM(
     always @ (posedge clk)
     begin
         case (cs)
-            5'd0:  row <= 7'd10;
-            5'd16: row <= row + 7'd1;
-            5'd17: row <= 7'd0;
+            6'd0:  row <= 7'd10;
+            6'd16: row <= row + 7'd1;
+            6'd17: row <= 7'd0;
+            6'd24: row <= row + 7'd1;
+            6'd25: row <= 7'd0;
+            6'd26: row <= row + 7'd1;
+            6'd31: row <= 7'd0;
 
             default:
                 row <= row;
@@ -190,7 +308,7 @@ module KeypadSampleFSM(
     // Register delay
     always @ (posedge clk)
     begin
-        if (cs == 5'd3)
+        if (cs == 6'd3)
             delay <= timer + 32'd100;
     end
 
@@ -199,21 +317,30 @@ module KeypadSampleFSM(
     begin
 
     
-        if (cs == 5'd1)
+        if (cs == 6'd1)
             /*verilator lint_off WIDTH*/
             vga_addr_reg <= row * 7'd80 + col;
 
-        if (cs == 5'd3)
+        if (cs == 6'd3)
             vga_addr_reg <= row * 7'd80 + col;
 
-        if (cs == 5'd11)
+        if (cs == 6'd11)
             vga_addr_reg <= row * 7'd80 + col;
 
-        if (cs == 5'd13)
+        if (cs == 6'd13)
             vga_addr_reg <= row * 7'd80 + col;
 
+        if (cs == 6'd19)
+            vga_addr_reg <= row * 7'd80 + col;
 
+        if (cs == 6'd21)
+            vga_addr_reg <= row * 7'd80 + col;
 
+        if (cs == 6'd27)
+            vga_addr_reg <= row * 7'd80 + col;
+
+        if (cs == 6'd29)
+            vga_addr_reg <= row * 7'd80 + col;
     
     end
 
